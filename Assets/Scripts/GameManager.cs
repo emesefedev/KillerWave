442d770802c9e20d3bbf479dfb74c7b3daa3b372
emesefedev <emesefedev@gmail.com>
2024-09-05 10:47:49 +0200

    directional light and IActorTemplate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private Light directionalLight;
     private Vector3 initialCameraPosition = new Vector3(0, 0, 300);
+
+    private Vector3 directionalLightRotation = new Vector3(50, -30, 0);
+    private Color directionalLightColor = new Color(0.596f, 0.8f, 1, 1);
 
     private void Start()
     {
         CameraSetup();
+        LightSetup();
     }
 
     private void CameraSetup() 
@@ -17,5 +22,11 @@ public class GameManager : MonoBehaviour
 
         mainCamera.clearFlags = CameraClearFlags.SolidColor;
         mainCamera.backgroundColor = Color.black;
+    }
+
+    private void LightSetup()
+    {
+        directionalLight.transform.eulerAngles = directionalLightRotation;
+        directionalLight.color = directionalLightColor;
     }
 }
