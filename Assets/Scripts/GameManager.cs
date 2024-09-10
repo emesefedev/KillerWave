ca871c5db10b962f4b01ee54cf08d8eb9616d8d3
emesefedev <emesefedev@gmail.com>
2024-09-10 14:46:32 +0200

    game manager is singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
     [SerializeField] private Camera mainCamera;
     [SerializeField] private Light directionalLight;
     private Vector3 initialCameraPosition = new Vector3(0, 0, -300);
@@ -9,11 +12,30 @@ public class GameManager : MonoBehaviour
     private Vector3 directionalLightRotation = new Vector3(50, -30, 0);
     private Color directionalLightColor = new Color(0.596f, 0.8f, 1, 1);
 
+    private void Awake()
+    {
+        CheckGameManagerIsInScene();
+    }
+
     private void Start()
     {
         CameraSetup();
         LightSetup();
     }
+
+    private void CheckGameManagerIsInScene()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else 
+        {
+            Destroy(this);
+        }
+    }
+
 
     private void CameraSetup() 
     {
