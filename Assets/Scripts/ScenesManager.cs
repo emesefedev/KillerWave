4b2c87510012d014e48fc88420b8248bf6359cf4
emesefedev <emesefedev@gmail.com>
2024-09-10 14:52:13 +0200

    create scenes manager script

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ScenesManager : MonoBehaviour
+{
+    public enum Scenes
+    {
+        BootUp,
+        Title,
+        Shop,
+        Level1,
+        Level2,
+        Level3,
+        GameOver
+    }    
+}
