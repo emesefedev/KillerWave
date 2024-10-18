16b1c6db135de7be67c55ab3b1ba2aeee423a09c
emesefedev <emesefedev@gmail.com>
2024-10-18 16:19:30 +0200

    feature(level3): boss animations

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Boss : MonoBehaviour 
+{
+	private void OnTriggerEnter(Collider other)
+    { 
+        if (other.gameObject.CompareTag("Player"))
+        {
+			Die(other.gameObject);
+        }
+    }
+
+	public void Die(GameObject other)
+    {
+        Destroy(other);
+    }
+}
---
+++ Assets/Scripts/BossTriggerBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class BossTriggerBox : MonoBehaviour 
+{
+	private GameObject timeLine;
+	
+    private void Start()
+	{
+		timeLine = GameObject.Find("Timeline");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (timeLine != null)
+        {
+            PlayableDirector playableDirector = timeLine.GetComponent<PlayableDirector>();
+            playableDirector.Play();
+        }
+	}
+}
