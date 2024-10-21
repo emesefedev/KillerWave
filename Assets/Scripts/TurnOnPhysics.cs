b7190fa90b6e435ab6951bd97bf3dbccd9bed0df
emesefedev <emesefedev@gmail.com>
2024-10-21 15:50:59 +0200

    feature(level3): cargo bulk physics

---
+++ Assets/Scripts/TurnOnPhysics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnPhysics : MonoBehaviour 
+{
+    [SerializeField] private float timeToSwitchPhysics = 38;
+	private float timer = 0;
+	private bool switchPhysicsOn = false;
+	
+    private void Update() 
+    {
+		if (Time.timeScale == 1)
+		{
+			timer += Time.deltaTime;	
+		}
+
+		if (timer > timeToSwitchPhysics && !switchPhysicsOn)
+		{
+			switchPhysicsOn = true;
+
+			for (int i = 0; i < this.transform.childCount; i++)
+			{
+				this.transform.GetChild(i).GetComponent<Rigidbody>().isKinematic = false;
+			}
+		}
+	}
+}
